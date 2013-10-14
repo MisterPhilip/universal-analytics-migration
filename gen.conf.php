25084db4790e006a128022b0b5c2fa45e91e1bd2
Philip Lawrence <philip@misterphilip.com>
2013-10-13 19:11:16 -0500

    Adding config file for easy contributing

---
+++ gen.conf.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'extensions'    => 'extensions',
+    'content'       => 'content',
+    'templates'     => 'templates',
+    'assets'        => ['assets'],
+    'global'        => 'global.php',
+    'local'         => 'local.php',
+    'src'           => $src,
+    'dest'          => $src . '/public',
+];
